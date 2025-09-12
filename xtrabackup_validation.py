63b1f5f33bfe7fc270f3389caf6bb20a873db229
NazRinRus <NazRinRus@gmail.com>
2025-09-12 15:10:58 +0000

    report

--- xtrabackup_validation.py
+++ xtrabackup_validation.py
@@ -7,7 +7,14 @@ logging.basicConfig(level=logging.INFO, filename="x_validation.log",filemode="w"
                     format="%(asctime)s %(levelname)s %(message)s")
 
 logging.info(f"Running validation script. List of clusters: {', '.join(CLUSTER_NAMES)}")
+
+val_durations = {}
+exit_codes = {}
+
 for cluster_name in CLUSTER_NAMES:
+    
+    # val_start_time = 
+    exit_code = 0
     # Если кластер активен, то выключаем его
     cluster_instance = MySQL_cluster(cluster_name)    
     logging.info(f"Working with the '{cluster_name}' cluster'")
@@ -17,6 +24,7 @@ for cluster_name in CLUSTER_NAMES:
             if cluster_instance.stop_cluster():
                 logging.info(f"MySQL service stoped. Cluster '{cluster_name}'")
         except subprocess.CalledProcessError as e:
+            exit_code = 1
             logging.error(e)
     else: logging.info(f"MySQL service is inactive")
         
@@ -25,6 +33,7 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.clear_data_dir():
             logging.info(f"Successfully cleared '{cluster_instance.mysql_data_dir}'")
     except subprocess.CalledProcessError as e:
+        exit_code = 1
         logging.error(e)
 
     # Копирование файлов бэкапа в директорию данных
@@ -32,6 +41,7 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.copy_backup_in_datadir():
             logging.info(f"Copying backup files '{cluster_name}' to data directory '{cluster_instance.mysql_data_dir}' completed successfully ")
     except subprocess.CalledProcessError as e:
+        exit_code = 1
         logging.error(e)
 
     # Восстановление данных
@@ -39,6 +49,7 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.xtrabackup_restore():
             logging.info(f"Cluster '{cluster_name}' recovery completed successfully")
     except subprocess.CalledProcessError as e:
+        exit_code = 1
         logging.error(e)
 
     # Запуск сервиса
@@ -46,6 +57,7 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.start_cluster():
             logging.info(f"Service 'mysql' - cluster '{cluster_name}' start successful")
     except subprocess.CalledProcessError as e:
+        exit_code = 1
         logging.error(e)
 
     # Снятие дампа
@@ -53,6 +65,7 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.dump_validation():
             logging.info(f"Taking dump from cluster '{cluster_name}' completed successfully")
     except Exception as e:
+        exit_code = 1
         logging.error(e)
 
     # Отключение сервиса
@@ -60,6 +73,7 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.stop_cluster():
             logging.info(f"Service 'mysql' - cluster '{cluster_name}' stop successful")
     except subprocess.CalledProcessError as e:
+        exit_code = 1
         logging.error(e)
 
     # Очистка директории с данными
@@ -67,6 +81,11 @@ for cluster_name in CLUSTER_NAMES:
         if cluster_instance.clear_data_dir():
             logging.info(f"Successfully cleared '{cluster_instance.mysql_data_dir}'")
     except subprocess.CalledProcessError as e:
+        exit_code = 1
         logging.error(e)
+
+    # val_stop_time = 
+    val_durations[cluster_name] = str(val_stop_time - val_start_time)
+    exit_codes[cluster_name] = exit_code
 
 logging.info(f"Script execution completed")
